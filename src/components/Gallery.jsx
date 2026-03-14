0b51398c20b5313aa89d1527b57f0c3cea746b46
Akansha-Martand <martandakansha@gmail.com>
2026-03-14 16:06:03 +0530

    Add Search filter using useCallback and useMemo

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,20 @@
 import useFetchPhotos from '../hooks/useFetchPhotos';
+import { useState, useMemo, UseCallback } from 'react';
+
+
 function Gallery() {
     const { photos, loading, error } = useFetchPhotos();
+    const { search, setSearch } = useState('');
+
+    const handleSearch = useCallback((e) => {
+        setSearch(e.target.value);
+    }, []);
+
+    const filterPhotos = useMemo(() => {
+        return photos.filter((photo) => photo.author.toLowerCase().includes(search.toLowerCase()));
+
+    }, [photos, search]);
+
 
     if (loading) {
         return <p className="text-center mt-10">Loading photos...</p>;
@@ -11,8 +25,9 @@ function Gallery() {
     }
 
     return (
+        <input type="text" placeholder='Search by author...' value={search} onChange={handleSearch} className="border p-2 rounded w-full mb-4"/>
         <div className="p-6 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            {photos.map((photo) => (
+            {filteredphotos.map((photo) => (
                 <div key={photo.id} className="border rounder-lg p-3">
                     <img src={photo.download_url} alt={photo.author} className="w-full h-48 object-cover rounded" />
                     <p className="mt-2 text-sm">{photo.author}</p>
