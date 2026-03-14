46093b9b616cd02ca4e2d399a16d73ffbc9fb24a
Akansha-Martand <martandakansha@gmail.com>
2026-03-14 17:08:46 +0530

    Implement favourites using useReducer with localStorage persistence

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,25 @@
 import useFetchPhotos from '../hooks/useFetchPhotos';
 import { useState, useMemo, UseCallback } from 'react';
+import { useReducer, useEffect } from "react";
+import { favouritesReducer } from "../reducers/favouritesReducer";
 
 
 function Gallery() {
     const { photos, loading, error } = useFetchPhotos();
     const { search, setSearch } = useState('');
+
+    const [favourites, dispatch] = useReducer(favouritesReducer, [], () => {
+        const stored = localStorage.getItem("favourites");
+        return stored ? JSON.parse(stored) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("favourites", JSON.stringify(favourites));
+    }, [favourites]);
+
+    function toggleFavourite(photo) {
+        dispatch({ type: "TOGGLE_FAVOURITE", payload: photo });
+    }
 
     const handleSearch = useCallback((e) => {
         setSearch(e.target.value);
@@ -25,8 +40,8 @@ function Gallery() {
     }
 
     return (
-        <input type="text" placeholder='Search by author...' value={search} onChange={handleSearch} className="border p-2 rounded w-full mb-4"/>
         <div className="p-6 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            <input type="text" placeholder='Search by author...' value={search} onChange={handleSearch} className="border p-2 rounded w-full mb-4" />
             {filteredphotos.map((photo) => (
                 <div key={photo.id} className="border rounder-lg p-3">
                     <img src={photo.download_url} alt={photo.author} className="w-full h-48 object-cover rounded" />
---
+++ src/reducers/favouritesReducers.js
@@ -0,0 +1,11 @@
+export function favouritesReducer(state, action) {
+    switch (action.type) {
+        case "TOGGLE_FAVOURITE":
+            const exists = state.find((p) => p.id === action.payload.id);
+            if (exists) {
+                return state.filter((p) => p.id !== action.payload.id);
+            }
+            return [...state, action.payload];
+        default:
+            return state;
+    }
